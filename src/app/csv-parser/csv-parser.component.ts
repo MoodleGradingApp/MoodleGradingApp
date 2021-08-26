2e8eb0b8876d81e18d7a98e551df7dc117325cbd
Emily Costa <emilycosta@dhcp82-194.calvin.edu>
2021-08-26 19:21:32 -0400

    WIP service

--- src/app/csv-parser/csv-parser.component.ts
+++ src/app/csv-parser/csv-parser.component.ts
@@ -15,10 +15,11 @@ export class CsvParserComponent {
   // feedback service
   // feedbackService: FeedbackService[] = [];
 
-  currentStudentIndex: number = -1;
-
   correctFile: boolean;
   emptyFile: boolean;
+
+  currentStudentIndex: number = -1;
+
   isRowSelected: boolean =  false;
   previousRow: number = 2;
 
@@ -29,63 +30,19 @@ export class CsvParserComponent {
 
   studentRow: string[] = ['i', 'name', 'email', 'timestamp', 'grade', 'feedback'];
 
-  constructor(private ngxCsvParser: NgxCsvParser, private feedbackService: FeedbackService) {
+  constructor(private feedbackService: FeedbackService) {
     // no-op
   }
 
   // Your applications input change listener for the CSV File
   fileChangeListener($event: any): void {
 
-    // Select the files from the event
-    const files = $event.srcElement.files;
+    // Select the file from the event
+    const file = $event.srcElement.files;
 
-    console.log("File size: ", files[0]["size"]);
+    console.log("File size: ", file[0]["size"]);
 
-    // Check for empty CSV file
-    if (files[0]["size"] > 3) {
-      this.emptyFile = false;
-    } else {
-      this.emptyFile = true;
-      this.correctFile =  false;
-      console.log("File is empty!")
-      return;
-    }
-
-    // reference: https://www.npmjs.com/package/ngx-csv-parser
-    // Parse the file you want to select for the operation along with the configuration
-    this.ngxCsvParser.parse(files[0], { header: this.header, delimiter: ',' })
-      .pipe().subscribe((result: Array<any>) => {
-        console.log('Parser Result', result);
-        this.csvRecords = result;
-
-        // check headers if correct CSV file
-        if (this.csvRecords[0]["Email address"] === undefined ||
-        this.csvRecords[0]["Feedback comments"] === undefined ||
-        this.csvRecords[0]["Full name"] === undefined ||
-        this.csvRecords[0]["Grade"] === undefined ||
-        this.csvRecords[0]["Grade can be changed"] === undefined ||
-        this.csvRecords[0]["Identifier"] === undefined ||
-        this.csvRecords[0]["Last modified (grade)"] === undefined ||
-        this.csvRecords[0]["Last modified (submission)"] === undefined ||
-        this.csvRecords[0]["Maximum Grade"] === undefined ||
-        this.csvRecords[0]["Online text"] === undefined ||
-        this.csvRecords[0]["Status"] === undefined) {
-          console.log("Wrong CSV File!");
-          this.correctFile = false;
-        } else {
-          this.correctFile = true;
-        }
-
-        if (this.correctFile === true) {
-          // return only Calvin username, get rid of '@students.calvin.edu'
-          console.log(this.csvRecords.length);
-          for (let i = 0; i < this.csvRecords.length; i++) {
-            this.csvRecords[i]["Email address"] = this.csvRecords[i]["Email address"].split("@", 1);
-          }
-        }
-      }, (error: NgxCSVParserError) => {
-        console.log('Error', error);
-      });
+    this.correctFile = this.feedbackService.parseFile(file);
   }
 
   ngOnInit(): void {
@@ -110,7 +67,6 @@ export class CsvParserComponent {
     console.log("DB Call: " + this.feedbackService.getStudents);
     this.currentStudentIndex = index;
     this.selectedStudent.emit(this.csvRecords[index]["Full name"]);
-    this.feedbackService.addToDb(this.csvRecords[1]["Full name"]);
     console.log("Added students: ");
     console.log(this.feedbackService.getStudents);
   }
--- src/app/feedback.service.ts
+++ src/app/feedback.service.ts
@@ -1,36 +1,98 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { string } from 'yargs';
+import { NgxCSVParserError, NgxCsvParser } from 'ngx-csv-parser';
 
-export interface Students {
-  "Email address": string,
-  "Feedback comments": string,
-  "Full name": string,
-  "Grade": string,
-  "Grade can be changed": string,
-}
+// export interface StudentInfo {
+//   "Email address": string,
+//   "Feedback comments": string,
+//   "Full name": string,
+//   "Grade": string,
+//   "Grade can be changed": string,
+// }
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
 
-  constructor() { }
+  constructor(private ngxCsvParser: NgxCsvParser) { }
 
-  students: Students[] = [];
+  // students: StudentInfo[] = [];
+  correctFile: boolean;
+  emptyFile: boolean;
+  header = true;
 
-  addToDb(student: Students) {
-    this.students.push(student);
+  csvRecords: Array<String>[] = [];
+
+  parseFile(fileName: any): boolean {
+
+        // Check for empty CSV file
+        if (fileName[0]["size"] > 3) {
+          this.emptyFile = false;
+        } else {
+          this.emptyFile = true;
+          this.correctFile =  false;
+          console.log("File is empty!")
+          return;
+        }
+
+        // reference: https://www.npmjs.com/package/ngx-csv-parser
+        // Parse the file you want to select for the operation along with the configuration
+        this.ngxCsvParser.parse(fileName[0], { header: this.header, delimiter: ',' })
+          .pipe().subscribe((result: Array<any>) => {
+            console.log('Parser Result', result);
+            this.csvRecords = result;
+
+            // check headers if correct CSV file
+            if (this.csvRecords[0]["Email address"] === undefined ||
+            this.csvRecords[0]["Feedback comments"] === undefined ||
+            this.csvRecords[0]["Full name"] === undefined ||
+            this.csvRecords[0]["Grade"] === undefined ||
+            this.csvRecords[0]["Grade can be changed"] === undefined ||
+            this.csvRecords[0]["Identifier"] === undefined ||
+            this.csvRecords[0]["Last modified (grade)"] === undefined ||
+            this.csvRecords[0]["Last modified (submission)"] === undefined ||
+            this.csvRecords[0]["Maximum Grade"] === undefined ||
+            this.csvRecords[0]["Online text"] === undefined ||
+            this.csvRecords[0]["Status"] === undefined) {
+              console.log("Wrong CSV File!");
+              this.correctFile = false;
+            } else {
+              this.correctFile = true;
+            }
+
+            if (this.correctFile === true) {
+              // return only Calvin username, get rid of '@students.calvin.edu'
+              console.log(this.csvRecords.length);
+              for (let i = 0; i < this.csvRecords.length; i++) {
+                this.csvRecords[i]["Email address"] = this.csvRecords[i]["Email address"].split("@", 1);
+              }
+            }
+            this.getStudents();
+          }, (error: NgxCSVParserError) => {
+            console.log('Error', error);
+          });
+
+    return this.correctFile
   }
 
+  updateFeedback(feedback: string, studentIndex: number) {
+    // update index student # feedback
+  }
+
+  // addToDb(student: Students) {
+  //   this.students.push(student);
+  // }
+
   getStudents() {
-    console.log(this.students);
-    return this.students;
+    console.log("getStudents() results: " + this.csvRecords);
+    // return this.students;
   }
 
   clearStudents() {
-    this.students = [];
-    return this.students;
+    // this.students = [];
+    // return this.students;
   }
 
 }
