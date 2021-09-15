1f9904c47c511a33f70a6fc0ad3c8023cc6a754f
Emily Costa <ecosta@michiganlabs.com>
2021-09-15 18:40:54 -0400

    remove csv child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
-import { ViewChild, } from '@angular/core';
+import { ViewChild, EventEmitter} from '@angular/core';
 import { Subject } from 'rxjs';
 import { CsvParserComponent } from './csv-parser/csv-parser.component';
+import { FeedbackService, StudentInfo } from './feedback.service';
 
 @Component({
   selector: 'app-root',
@@ -11,24 +12,26 @@ import { CsvParserComponent } from './csv-parser/csv-parser.component';
 })
 
 export class AppComponent {
+
+  // What is this????? Ask Michael
   title(title: any) {
     throw new Error('Method not implemented.');
   }
 
-  // listen to parent event 'next' or 'previous' student button clicked
-  eventsSubject: Subject<void> = new Subject<void>();
-
   public feedbackArray: FormArray;
   public feedbackForm: FormGroup;
+  validFile: boolean = true;
   currentStudentName: String;
-
-  // member variable, type of child component class
-  @ViewChild('next') csvChildComponent:CsvParserComponent
+  currentStudentIndex: number = -1;
+  isRowSelected: boolean =  false;
+  previousRow: number = 2;
+  selectedUser: Array<String>[] = [];
+  studentRow: string[] = ['i', 'name', 'email', 'timestamp', 'grade', 'feedback'];
 
   csvRecords: any[] = [];
   header: boolean = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private feedbackService: FeedbackService) {
     this.feedbackForm = this.fb.group({
        feedbackArray: this.fb.array([ this.createFeedback() ]),
     });
@@ -36,6 +39,61 @@ export class AppComponent {
 
   get feedbackControls() {
     return this.feedbackForm.get('feedbackArray')['controls'];
+  }
+
+  fileChangeListener($event: any) {
+
+    // Select the file from the event
+    const file = $event.srcElement.files;
+
+    console.log("File size: ", file[0]["size"]);
+
+    // wait for this to return
+    this.feedbackService.parseFile(file);
+
+    this.csvRecords = []
+
+    this.csvRecords = this.feedbackService.fillChart();
+
+    this.validFile = this.feedbackService.correctFile
+
+    console.log("correct File? ", this.validFile)
+  }
+
+  ngOnInit(): void {
+    // no-op
+  }
+
+  highlightRow(row:number) {
+    // add 'selected' class to tr element
+    var trs = document.querySelectorAll("tr.csv-data");
+    if (this.isRowSelected === false) {
+      this.isRowSelected = true;
+    } else {
+      console.log("Previous Row: " + (this.previousRow));
+      trs[this.previousRow].classList.remove("selected");
+    }
+    trs[row].classList.add("selected");
+    console.log("Row: " + (row));
+    this.previousRow = row;
+  }
+
+  rowSelected(index:number) {
+    this.currentStudentIndex = index;
+    this.currentStudentName = this.csvRecords[index].fullName;
+  }
+
+  studentParser(incriment: number): void {
+    if (this.currentStudentIndex === 0 && incriment === -1 || this.currentStudentIndex === this.csvRecords.length-1 && incriment === 1) {
+      return
+    }
+    this.currentStudentIndex += incriment;
+    this.rowSelected(this.currentStudentIndex);
+    this.highlightRow(this.currentStudentIndex);
+    console.log("Current Student: " + this.csvRecords[this.currentStudentIndex]["fullName"]);
+    this.validFile = this.feedbackService.correctFile
+
+    console.log("DELAY: correct File? ", this.validFile)
   }
 
   createFeedback(): FormGroup {
@@ -53,17 +111,11 @@ export class AppComponent {
     this.feedbackArray.push(this.createFeedback());
   }
 
-  currentStudent(studentData:string):void {
-    this.currentStudentName = studentData;
-  }
-
   nextStudent(): void {
-    // call function from child component
-    this.csvChildComponent.studentParser(1);
+    this.studentParser(1);
   }
 
   previousStudent(): void {
-    // call function from child component
-    this.csvChildComponent.studentParser(-1);
+    this.studentParser(-1);
   }
 }
