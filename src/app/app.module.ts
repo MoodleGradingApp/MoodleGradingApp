6ac2d2e44d4b623f92f624e8004a375a235c1d51
Victor Norman <vtn2@calvin.edu>
2022-03-03 15:54:19 -0500

    try adding sentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule} from '@angular/core';
+import { APP_INITIALIZER, ErrorHandler, NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from "@angular/router";
+import * as Sentry from "@sentry/angular";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +24,24 @@ import { NgApexchartsModule } from 'ng-apexcharts';
     NgxCsvParserModule,
     NgApexchartsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: true,
+      }),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => { },
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as Sentry from "@sentry/angular";
+import { BrowserTracing } from '@sentry/tracing';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -7,6 +9,21 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+
+Sentry.init({
+  dsn: "https://983f6082f2574af3956dff4ab2fc58b8@o1158351.ingest.sentry.io/6241449",
+  integrations: [
+    new BrowserTracing({
+      tracingOrigins: ["localhost", "https://yourserver.io/api"],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
